0717446b158d14d67afd09aaba94eb379aaa1607
Petar Dragojlovic <petardragojlovicbb@gmail.com>
2023-09-02 11:27:34 +0200

    working hours

--- src/components/Header components/DesktopNav/TopNav/TopNavbar.jsx
+++ src/components/Header components/DesktopNav/TopNav/TopNavbar.jsx
@@ -7,21 +7,23 @@ function TopNavbar() {
   return (
     <div className="topNav">
       <div className="topNav__contacts">
-        <a className="topNav__contacts__number" href="tel:+4733378901">
+        {/* <a className="topNav__contacts__number" href="tel:+4733378901">
           <FaPhoneAlt className="topNav__contacts__phonelIcon" />
-          {/* Call for free consulatation:  */}
+          Call for free consulatation: 
           +47 333 78 901
-        </a>
-        <a className="topNav__contacts__email" href="mailto: abc@example.com">
+        </a> */}
+        {/* <a className="topNav__contacts__email" href="mailto: abc@example.com">
           <FaEnvelope className="topNav__contacts__emailIcon" />
           abc@example.com
-        </a>
-      </div>
-      <div className="topNav__rightSection">
+        </a> */}
         <span className="topNav__rightSection__workTime">
           <BiTime className="topNav__rightSection__workTime__icon" />
-          Monday to Friday 8am - 4pm
+          <span>Working hours:&nbsp;&nbsp;</span>
+          <span>Monday to Friday&nbsp;</span>
+          <span>8am - 4pm</span>
         </span>
+      </div>
+      <div className="topNav__rightSection">
         <Translation />
       </div>
     </div>
